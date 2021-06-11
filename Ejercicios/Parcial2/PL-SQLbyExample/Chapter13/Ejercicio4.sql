94a015d79235c253be06e44d7a28e912306f40d0
OscarMendoza-afk <73726072+OscarMendoza-afk@users.noreply.github.com>
2021-06-11 02:27:37 -0500

    Chapter 13

---
+++ Ejercicios/Parcial2/PL-SQLbyExample/Chapter13/Ejercicio4.sql
@@ -0,0 +1,24 @@
+spool C:\Users\ozkr_\OneDrive\Documentos\Github\Distributed_DataBase\Ejercicios\Parcial2\PL-SQLbyExample\Chapter13\salida\ejercicio4chapter13
+
+/*
+rem **********************************************************
+rem * Distributed DataBase, ESCOM. Ciclo 2021-2              * 
+rem * Elaborado por:                                         *
+rem * Cort�s Castilllo Daniela y Mendoza Cuellar Jos� Oscar  *                 
+rem * Realizado el 16 de mayo  de 2021                       *
+rem * ROSENZWEIG,B &  RAKHIMOV,E (2009).                     *
+rem *Oracle� PL/SQL�by Example,Boston,MA,USA:Perarson.      *
+rem **********************************************************
+*/
+
+set colsep '|='
+set describe linenum on
+SET PAGESIZE 99;
+SET LINESIZE 150
+alter session set NLS_DATE_FORMAT = 'DD-MON-YYYY';
+alter session set NLS_DATE_LANGUAGE = 'ENGLISH';
+SET SERVEROUTPUT ON;
+
+
+
+spool OFF;
